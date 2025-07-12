3caeacade44a489ffa217ef65d6d280d41e28180
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-07-12 12:59:28 +0530

    Route Groups part4-adding back button in about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,11 @@
+import Link from "next/link";
+
 const About = ()=>{
     return(
+        <>
         <h1>Hello About!</h1>
+        <Link href='/'>Back To Home</Link>
+        </>
     );
 };
 export default About;
