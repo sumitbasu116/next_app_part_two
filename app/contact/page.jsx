12fbb0ede06fa0fa4dacc0381f5b6095e23a88c0
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-07-12 08:44:56 +0530

    Link Component part0-basic structure set up

---
+++ app/contact/page.jsx
@@ -0,0 +1,8 @@
+const Contact=()=>{
+  return(
+    <>
+    <h1>Hello Contact!!</h1>
+    </>
+  )
+};
+export default Contact;
---
+++ app/service/page.jsx
@@ -0,0 +1,8 @@
+const Service=()=>{
+  return(
+    <>
+    <h1>Hello Service!!</h1>
+    </>
+  )
+};
+export default Service;
