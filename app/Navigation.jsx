b57521668e6245661737078092a3189bbc542cf3
Sumit Basu <sumitbasu@Sumits-MacBook-Pro-2.local>
2025-07-12 08:48:29 +0530

    Link Component part1-navigation to show in Home option 1

--- app/Navigation.jsx
+++ app/Navigation.jsx
@@ -1,24 +1,6 @@
 export default function Navigation(){
 
     return(
-        <header>
-            <div className="">Sumit Logo</div>
-        <nav>
-            <ul>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/about">About</a>
-                </li>
-                <li>
-                    <a href="/contact">Contact</a>
-                </li>
-                <li>
-                    <a href="/service">Service</a>
-                </li>
-            </ul>
-        </nav>
-        </header>
+        <></>
     );
 };
--- app/page.js
+++ app/page.js
@@ -1,6 +1,25 @@
 const Home=()=>{
   return(
     <>
+    <header>
+            <div className="">Sumit Logo</div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
+                <li>
+                    <a href="/service">Service</a>
+                </li>
+            </ul>
+        </nav>
+        </header>
     <h1>Hello World Next JS!!</h1>
     </>
   )
