411a37c102044c92de20109e77f031d92b615ccc
Sumit <sumitbasu116@gmail.com>
2025-07-13 12:07:21 +0530

    font in Next JS part1-changing text font inRoboto

--- app/(users)/layout.js
+++ app/(users)/layout.js
@@ -1,11 +1,16 @@
 import "../globals.css";
 import Navigation from "../components/Navigation";
+import {Roboto} from "next/font/google";
+
+const roboto = Roboto({
+  subsets:["latin"]
+});
 
 function RootLayout({children}){
 
   return(
     <html>
-      <body>
+      <body className={roboto.className}>
         <Navigation/>
         {children}
         </body>
