962d4d6a1058463058ed06f26854c1f0deac8258
Sumit <sumitbasu116@gmail.com>
2025-07-13 14:43:54 +0530

    Metadata part3-adding complete metadata including icon

--- app/(users)/contact/page.jsx
+++ app/(users)/contact/page.jsx
@@ -1,3 +1,11 @@
+
+export const metadata={
+  title: "Contact Page",
+  description:"This is my Contact Page",
+  authors:[{name:"Sumit"},{name:"Sumit Technical",url:"sumitbasu.com"}],
+  keywords:["nextjs","fullstack","reactjs"]
+}
+
 const Contact=()=>{
   return(
     <>
--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,68 @@
 import "./globals.css";
-import {Roboto} from "next/font/google";
+import { Roboto } from "next/font/google";
 import { Work_Sans } from "next/font/google";
 
+export const metadata = {
+  title: {
+    default: "Sumit Technical | Learn Web Development",
+    template: "%s | Sumit Technical",
+  },
+  description:
+    "Free tutorials on React.js, Next.js, and web development by Sumit Technical.",
+  keywords: ["React", "Next.js", "Web Development", "Sumit Technical"],
+  icons: {
+    icon: "/images/kody.ico"
+  },
+  metadataBase: new URL("https://sumittechical.com"),
+  openGraph: {
+    title: "Sumit Technical",
+    description: "Join the best web dev tutorials!",
+    url: "https://yourdomain.com",
+    siteName: "Sumit Technical",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Sumit Technical Logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Sumit Technical",
+    description: "Learn Web Dev from scratch with Sumit Technical!",
+    creator: "@sumittechnical",
+    images: ["/og-image.png"],
+  },
+};
+
 const roboto = Roboto({
-  subsets:["latin"],
-  variable:"--font-roboto"
+  subsets: ["latin"],
+  variable: "--font-roboto"
 });
 
 const robotoWeight = Roboto({
-  subsets:["latin"],
-  variable:"--font-roboto-weight"
+  subsets: ["latin"],
+  variable: "--font-roboto-weight"
 });
 
 const workSans = Work_Sans(
   {
-    subsets:["latin"],
-    variable:"--font-work-sans",
-    weight:"600"
+    subsets: ["latin"],
+    variable: "--font-work-sans",
+    weight: "600"
   }
 )
 
-function RootLayout({children}){
+function RootLayout({ children }) {
 
-  return(
+  return (
     <html>
       <body className={`${roboto.variable} ${workSans.variable} ${robotoWeight.variable}`}>
         {children}
-        </body>
+      </body>
     </html>
   );
 };
