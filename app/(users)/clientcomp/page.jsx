e8eea57643a29193a2e33ede819cf7c855162816
Sumit <sumitbasu116@gmail.com>
2025-07-13 20:25:47 +0530

    Server vs Client Component part4-fetching and showing data in client component

--- app/(users)/clientcomp/page.jsx
+++ app/(users)/clientcomp/page.jsx
@@ -1,9 +1,40 @@
 'use client'
-const ClientComponent = ()=>{
-    return(
+
+import { useEffect, useState } from "react";
+
+const URL = "https://jsonplaceholder.typicode.com/posts";
+
+const ClientComponent = () => {
+
+    const [postData, setPostData] = useState([]);
+
+    useEffect(()=>{
+      const fetchData = async  () => {
+            const res = await fetch(URL);
+            const data = await res.json();
+            console.log(data);
+
+            setPostData(data);
+            return data;
+        };
+        fetchData();
+     } ,[]);
+
+    return (
         <>
-        <h1>Hello ClientComponent!</h1>
-        <button className="bg-amber-400 text-black p-5" onClick={()=>alert('Hii')}>Click Me</button>
+            <h1>Hello ClientComponent!</h1>
+            <button className="bg-amber-400 text-black p-5" onClick={() => alert('Hii')}>Click Me</button>
+
+            <ul className="grid grid-cols-3 gap-5">
+                {
+                    postData.map(
+                        (currElem,index)=>{
+                            return <li key={index}>{currElem.body}</li>
+                        }
+                    )
+                }
+            </ul>
+
         </>
     );
 };
