0df03a7069f63c17c2ef7c0729df5130c5d167ee
Sumit <sumitbasu116@gmail.com>
2025-07-13 20:04:29 +0530

    Server vs Client Component part3-removed use client from servercomp as we are not suppose use that in the server component, otherwise, it will not be a server component

--- app/(users)/servercomp/page.jsx
+++ app/(users)/servercomp/page.jsx
@@ -1,9 +1,7 @@
-'use client'
 const ServerComponent = ()=>{
     return(
         <>
         <h1>Hello ServerComponent!</h1>
-        <button className="bg-amber-400 text-black p-5" onClick={()=>alert('Hii ServerComponent')}>Click Me</button>
         </>
     );
 };
