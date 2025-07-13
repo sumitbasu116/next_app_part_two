0e810d519020706b1709b0eec21df326e8ba514a
Sumit <sumitbasu116@gmail.com>
2025-07-13 12:16:08 +0530

    font in Next JS part2-changing font from Roboto to Work Sans

--- app/(users)/layout.js
+++ app/(users)/layout.js
@@ -1,16 +1,23 @@
 import "../globals.css";
 import Navigation from "../components/Navigation";
 import {Roboto} from "next/font/google";
+import { Work_Sans } from "next/font/google";
 
 const roboto = Roboto({
   subsets:["latin"]
 });
 
+const workSans = Work_Sans(
+  {
+    subsets:["latin"]
+  }
+)
+
 function RootLayout({children}){
 
   return(
     <html>
-      <body className={roboto.className}>
+      <body className={workSans.className}>
         <Navigation/>
         {children}
         </body>
