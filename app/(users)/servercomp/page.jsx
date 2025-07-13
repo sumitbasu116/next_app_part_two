3f6d24c360917f33e88671606dc9f49c1c7fbb05
Sumit <sumitbasu116@gmail.com>
2025-07-13 20:36:55 +0530

    Server vs Client Component part5-fetching and showing data in server component

--- app/(users)/servercomp/page.jsx
+++ app/(users)/servercomp/page.jsx
@@ -1,7 +1,24 @@
-const ServerComponent = ()=>{
-    return(
+
+const URL = "https://jsonplaceholder.typicode.com/posts";
+const ServerComponent = async () => {
+
+    const res = await fetch(URL);
+    const data = await res.json();
+    console.log(data);
+
+    return (
         <>
-        <h1>Hello ServerComponent!</h1>
+            <h1> Server Component!</h1>
+            <ul className="grid grid-cols-3 gap-5">
+                {
+                    data.map(
+                        (currElem,index)=>{
+                            return <li key={index}>{currElem.body}</li>
+                        }
+                    )
+                }
+            </ul>
+
         </>
     );
 };
