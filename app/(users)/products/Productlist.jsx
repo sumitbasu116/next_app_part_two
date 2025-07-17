0996cb5f1feeeaa38af66d22c155e0da1a98ad6c
Sumit <sumitbasu116@gmail.com>
2025-07-17 11:24:09 +0530

    useSearchParams part1-get function to get each params

---
+++ app/(users)/products/Productlist.jsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useSearchParams } from "next/navigation";
+
+const ProductList = ()=>{
+
+    const searchParamData = useSearchParams();
+
+    console.log('inside',searchParamData);
+    
+    const page=searchParamData.get('page');
+    const category = searchParamData.get('category');
+    console.log('page:',page+" category:",category);
+
+    return(
+        <h1>Hello Product List</h1>
+    );
+}
+
+export default ProductList;
--- app/(users)/products/page.jsx
+++ app/(users)/products/page.jsx
@@ -1,12 +1,16 @@
+import ProductList from "./Productlist";
 
 const Products = async ({searchParams})=>{
     const searchParamsData = await searchParams;
     const category = searchParamsData?.category || "all";
     const sort = searchParamsData?.sort || "default";
     const page=searchParamsData?.page || 1;
+
+    console.log('outer',searchParamsData);
     
     return(
         <div>
+            <ProductList/>
             Showing {category} Products, sorted by {sort}, page {page}
         </div>
     );
