c403bf24aa6260158188ac390eff20510836c3c9
Sumit <sumitbasu116@gmail.com>
2025-07-24 11:21:10 +0530

    Auto Loading Animation with loading.jsx basic coding

--- app/(users)/datafetch/servercomp/page.jsx
+++ app/(users)/datafetch/servercomp/page.jsx
@@ -6,6 +6,16 @@ const DataFetchServer = async (props) => {
 
     const userName = searchParams.name;
 
+    await new Promise(
+      (resolve)=>{
+        setTimeout(
+          ()=>{
+            resolve();
+          },3000
+        );
+      }
+    );
+
     if (!userName) {
     return (
       <div className="min-h-screen bg-gradient-to-br from-purple-50 via-blue-50 to-indigo-100 flex items-center justify-center p-4">
