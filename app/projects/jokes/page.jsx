1f697fa3fc2640537d33f80a1392288e2de4fd6c
Sumit <sumitbasu116@gmail.com>
2025-07-24 11:58:51 +0530

    project jokes step2-fetching the API data

--- app/projects/jokes/page.jsx
+++ app/projects/jokes/page.jsx
@@ -3,7 +3,14 @@
 import { useEffect } from "react";
 
 const RandomJokes = ()=>{
+    const URL = 'https://official-joke-api.appspot.com/random_joke';
+    const fetchRandomJokes = async ()=>{
+        const res = await fetch(URL);
+        const data = await res.json();
 
+        console.log(data);
+        
+    }
     useEffect(
         ()=>{
             fetchRandomJokes();
