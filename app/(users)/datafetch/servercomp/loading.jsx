87693ffd8fe806c3f44d2afa4a8e852ad75ced11
Sumit <sumitbasu116@gmail.com>
2025-07-24 11:50:10 +0530

    Auto Loading Animation - showing loading with sand clock

--- app/(users)/datafetch/servercomp/loading.jsx
+++ app/(users)/datafetch/servercomp/loading.jsx
@@ -1,7 +1,17 @@
-const Loading = ()=>{
-    return(
+const Loading = () => {
+    return (
         <div className="loading-section">
-            <h1>Loading</h1>
+            <div className="hourglassBackground">
+                <div className="hourglassContainer">
+                    <div className="hourglassCurves"></div>
+                    <div className="hourglassCapTop"></div>
+                    <div className="hourglassGlassTop"></div>
+                    <div className="hourglassSand"></div>
+                    <div className="hourglassSandStream"></div>
+                    <div className="hourglassCapBottom"></div>
+                    <div className="hourglassGlass"></div>
+                </div>
+            </div>
         </div>
     );
 }
