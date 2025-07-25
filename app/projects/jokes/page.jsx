dbf502c27ed3beeba264506c1bf2831256dd3389
Sumit <sumitbasu116@gmail.com>
2025-07-25 09:27:59 +0530

    project jokes step5-show jokes and reveal punchline button

--- app/projects/jokes/page.jsx
+++ app/projects/jokes/page.jsx
@@ -2,29 +2,37 @@
 
 import { useEffect, useState } from "react";
 
-const RandomJokes = ()=>{
-    
-    const [randomJokes,setRandomJokes] = useState({});
+const RandomJokes = () => {
+    const [showJokes, setShowJokes] = useState(true);
+    const [randomJokes, setRandomJokes] = useState({});
 
     const URL = 'https://official-joke-api.appspot.com/random_joke';
-    const fetchRandomJokes = async ()=>{
+    const fetchRandomJokes = async () => {
         const res = await fetch(URL);
         const data = await res.json();
 
         console.log(data);
-        
+
         setRandomJokes(data);
     }
     useEffect(
-        ()=>{
+        () => {
             fetchRandomJokes();
-        },[]
+        }, []
     );
 
-    return(
+    return (
         <>
-        <p>{randomJokes.setup}</p>
-        <h2>{randomJokes.punchline}</h2>
+            <p>{randomJokes.setup}</p>
+            
+            {
+                showJokes ? (
+                    <button onClick={()=>setShowJokes(false)}>Reveal</button>
+                ) : (
+                    <div><h2>{randomJokes.punchline}</h2>
+                    <button onClick={()=>setShowJokes(true)}>Hide Punchline</button></div>
+                )
+            }
         </>
     );
 };
