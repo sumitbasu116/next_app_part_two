ce238b731505a7926e1f02ac31cc4d54ca5b51f5
Sumit <sumitbasu116@gmail.com>
2025-07-27 00:02:34 +0530

    Creating static page

---
+++ app/(users)/rendering/dynamic/page.jsx
@@ -0,0 +1,23 @@
+import { db } from "@/config/db.jsx";
+const StaticPage = async () => {
+    const [doctors] = await db.execute("select * from Doctor");
+    console.log('Dynamic Page');
+    
+    return (
+
+        <>
+            <h1>Hello Database</h1>
+            <div>
+                <ul>
+                {
+                    doctors.map((doctor)=>{
+                      return <li key={doctor.id}>{doctor.first_name}</li> 
+                    })
+                }
+                </ul>
+            </div>
+        </>
+    );
+}
+
+export default StaticPage;
